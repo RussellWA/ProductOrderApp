91a1bb28850a14725ad3ab2582445213ac01f763
Russell <gamingrussellwa@gmail.com>
2025-04-25 22:25:13 +0700

    [Added] Initialize Spring Project
    - Lots of files

---
+++ src/test/java/com/ProductOrderApp/ProductOrderApp/ProductOrderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProductOrderApp.ProductOrderApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOrderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
